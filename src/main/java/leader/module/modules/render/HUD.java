8e428f8f6ec3562e7b8fc3251f90f2a5dfa8371b
woshijiejue <3119754912@qq.com>
2026-07-22 14:11:42 +0800

    Fix a bug

--- src/main/java/leader/module/modules/render/HUD.java
+++ src/main/java/leader/module/modules/render/HUD.java
@@ -332,30 +332,30 @@ public class HUD extends Module {
                     float barY1 = bgY1 + barlessVal;
                     float barY2 = bgY2 - barlessVal;
                     if (barModeVal == 0) {
-                        float bw = 2.0F;
                         boolean alignLeft = this.posX.getValue() == 0;
                         if (alignLeft) {
+                            float edge = bgX1;
                             if (this.shadow.getValue()) {
-                                RenderUtil.drawRect(sx - 3.0F, barY1, sx - 2.0F, barY2, color);
-                                RenderUtil.drawRect(sx - 2.0F, barY1, sx - 0.5F, barY2, (color & 16579836) >> 2 | color & 0xFF000000);
+                                RenderUtil.drawRect(edge - 3.0F, barY1, edge - 2.0F, barY2, color);
+                                RenderUtil.drawRect(edge - 2.0F, barY1, edge + 0.5F, barY2, (color & 16579836) >> 2 | color & 0xFF000000);
                             } else {
-                                RenderUtil.drawRect(sx - 2.0F, barY1, sx - 0.5F, barY2, color);
+                                RenderUtil.drawRect(edge - 2.0F, barY1, edge + 0.5F, barY2, color);
                             }
                         } else {
+                            float edge = bgX2;
                             if (this.shadow.getValue()) {
-                                RenderUtil.drawRect(sx + 0.5F, barY1, sx + 2.0F, barY2, (color & 16579836) >> 2 | color & 0xFF000000);
-                                RenderUtil.drawRect(sx + 2.0F, barY1, sx + 3.0F, barY2, color);
+                                RenderUtil.drawRect(edge - 0.5F, barY1, edge + 2.0F, barY2, (color & 16579836) >> 2 | color & 0xFF000000);
+                                RenderUtil.drawRect(edge + 2.0F, barY1, edge + 3.0F, barY2, color);
                             } else {
-                                RenderUtil.drawRect(sx + 0.5F, barY1, sx + 2.0F, barY2, color);
+                                RenderUtil.drawRect(edge - 0.5F, barY1, edge + 2.0F, barY2, color);
                             }
                         }
                     } else if (barModeVal == 1) {
-                        float bw = 1.0F;
                         boolean alignLeft = this.posX.getValue() == 0;
                         if (alignLeft) {
-                            RenderUtil.drawRect(bgX2, barY1, bgX2 + bw, barY2, color);
+                            RenderUtil.drawRect(bgX2 - 0.5F, barY1, bgX2 + 1.0F, barY2, color);
                         } else {
-                            RenderUtil.drawRect(bgX1 - bw, barY1, bgX1, barY2, color);
+                            RenderUtil.drawRect(bgX1 - 1.0F, barY1, bgX1 + 0.5F, barY2, color);
                         }
                     } else if (barModeVal == 2) {
                         float bw = 1.0F;
